6f0b3f06eb3ec2ef5a76d40d2b80f2292c589909
albert203 <albert.jordaan@outlook.com>
2026-03-24 12:45:03 +1100

    intial node setup and express server

---
+++ src/server.ts
@@ -0,0 +1,36 @@
+import express from 'express'; // js runtime
+import path from 'path';
+
+import dotenv from 'dotenv';
+dotenv.config(); // loads .env file contents
+
+// create express instance
+const app = express();
+
+// assign the folder to serve static files
+app.use(express.static(path.join(__dirname, 'public')));
+
+// assign the view engine to ejs
+app.set('view engine', 'ejs');
+
+// route for home page
+app.get('/', (req, res) => {
+  res.render('index');
+});
+
+// routes for the other pages
+app.get('/resume', (req, res) => {
+  res.render('resume');
+});
+
+app.get('/projects', (req, res) => {
+  res.render('projects');
+});
+
+app.get('/contact', (req, res) => {
+  res.render('contact');
+});
+
+app.listen(process.env.PORT, () => {
+  console.log(`Server is running on port ${process.env.PORT}`);
+});
