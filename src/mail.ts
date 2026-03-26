9cc207a202504d491e6d83636e4f645af552d040
albert203 <albert.jordaan@outlook.com>
2026-03-26 13:51:23 +1100

    update to use resend api instead

--- src/mail.ts
+++ src/mail.ts
@@ -1,41 +1,19 @@
-import nodemailer, { TransportOptions } from 'nodemailer'
+import { Resend } from 'resend';
+import dotenv from 'dotenv';
+dotenv.config();
 
-// https://nodemailer.com/
+const resend = new Resend(process.env.RESEND_API_KEY);
 
-// Create a transporter using SMTP
-const transporter = nodemailer.createTransport({
-  host: "smtp.gmail.com",
-  port: 587,
-  secure: false, // use STARTTLS (upgrade connection to TLS after connecting)
-  auth: {
-    user: process.env.SMTP_USER,
-    pass: process.env.SMTP_PASS,
-  },
-  family: 4,
-} as any);
-
-export async function sendEmail(name:string, email:string, subject: string, message: string) {
-    try {
-        const result = await transporter.sendMail({
-            from: `"Albert Jordaan" <${process.env.SMTP_USER}>`, // ✅ Added name format
-            to: process.env.SMTP_USER,
-            replyTo: email,
-            subject: `Portfolio Site: ${subject}`,
-            text: message,
-            html: `<h2>from: ${name}</h2><br>
-                   <p>user email: ${email}</p><br>
-                   <p>subject: ${subject}</p><br>
-                   <p>message: ${message}</p>`,
-        });
-        console.log('Email sent correctly:', result.messageId);
-        return result;
-    } catch (err: any) {
-        console.error('error sending email:', {
-            message: err.message,
-            code: err.code,
-            command: err.command,
-            response: err.response,
-        });
-        throw err; 
-    }
+export async function sendEmail(name: string, email: string, subject: string, message: string) {
+  return resend.emails.send({
+    from: 'Albert Jordaan <onboarding@resend.dev>',
+    to: process.env.EMAIL_TO as string, // email I am sending to (aka me)
+    replyTo: email, // the other user email
+    subject: `Portfolio: ${subject}`,
+    text: message,
+    html: `<h2>From: ${name}</h2>
+           <p>Email: ${email}</p>
+           <p>Subject: ${subject}</p>
+           <p>Message: ${message}</p>`,
+  });
 }
