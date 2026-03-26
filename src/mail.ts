628078226e2dee2eaba692b24a5cb8c680f1cd8c
albert203 <albert.jordaan@outlook.com>
2026-03-26 13:27:27 +1100

    testing if ipv4 issue

--- src/mail.ts
+++ src/mail.ts
@@ -1,4 +1,4 @@
-import nodemailer from 'nodemailer'
+import nodemailer, { TransportOptions } from 'nodemailer'
 
 // https://nodemailer.com/
 
@@ -11,7 +11,8 @@ const transporter = nodemailer.createTransport({
     user: process.env.SMTP_USER,
     pass: process.env.SMTP_PASS,
   },
-});
+  family: 4,
+} as any);
 
 export async function sendEmail(name:string, email:string, subject: string, message: string) {
     try {
